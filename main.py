98525edcffcef0f6c05b909c733450f7c10175be
Camilo <camilo.perezvr@gmail.com>
2017-08-26 11:56:43 -0500

    4 botones

--- main.py
+++ main.py
@@ -28,5 +28,36 @@ lblinfo.grid(row=0,column=0)
 
 def btnClick(numero):
     global operador
+    operador = operador+str(numero)
+    text_input.set(operador)
+
+def btnEval():
+    global operador
+    result = str(eval(operador))
+    text_input.set(result)
+    operador = ""
+
+txtDisplay = Entry(frame,
+    font=("SHOWCARD GOTHIC", 50, 'bold'),
+    textvariable=text_input, bg = "black")
+
+txtDisplay.grid(columnspan = 4)
+
+btn1 = Button(
+    frame,
+    font=("SHOWCARD GOTHIC", 50, 'bold'),
+    command = lambda:btnClick(1)).grid(row=2,column=0)
+btn2 = Button(
+    frame,
+    font=("SHOWCARD GOTHIC", 50, 'bold'),
+    command = lambda:btnClick(2)).grid(row=2,column=1)
+Addition = Button(
+    frame,
+    font=("SHOWCARD GOTHIC", 50, 'bold'),
+    command = lambda:btnClick('+')).grid(row=2,column=3)
+Equals = Button(
+    frame,
+    font=("SHOWCARD GOTHIC", 50, 'bold'),
+    command = lambda:btnEval()).grid(row=3,column=0)
 
 root.mainloop()
